877e475a6484dc6a9e95652ffb7d8b3f4c9b9c1a
zmen5809 <zmen5809@gmail.com>
2019-05-16 17:47:40 +1000

    .........

--- type3_stage4.py
+++ type3_stage4.py
@@ -1,14 +1,12 @@
 from Music import Music
 from ml_utils import *
 import os
-import tensorflow as tf
-import tensorflow_hub as hub
+import numpy as np
+
+np.set_printoptions(threshold=np.inf)
 import time
+
 from datetime import datetime
-
-
-model_url = "https://tfhub.dev/google/universal-sentence-encoder/2"
-embed = hub.Module(model_url)
 
 os.environ['JAVA_HOME'] = "/Library/Java/JavaVirtualMachines/jdk1.8.0_211.jdk/Contents/Home"
 
@@ -19,75 +17,75 @@ os.environ["PYSPARK_SUBMIT_ARGS"] = pyspark_submit_args
 if __name__ == "__main__":
     start = time.time()
     music = Music()
-    select_PID = music.Products.sortBy(lambda x: x[1], ascending=False).take(10)[0][0]
+    select_PID = music.Products.sortBy(lambda x: x[1], ascending=False).take(10)[9][0]
     print(select_PID)
 
     Positive_Reviews = music.og \
         .filter(lambda x: x.split("\t")[3] == select_PID) \
         .filter(lambda x: int(x.split("\t")[7]) >= 4) \
         .flatMap(lambda x: splitSentence(x.split("\t")[13]))
-
-    print(Positive_Reviews.count())
+    num_Postive_Sentence = Positive_Reviews.count()
+    print("total num of sentences in positive reviews:", num_Postive_Sentence)
 
     Negative_Reviews = music.og \
         .filter(lambda x: x.split("\t")[3] == select_PID) \
         .filter(lambda x: int(x.split("\t")[7]) <= 2) \
-        .flatMap(lambda x: splitSentence(x.split("\t")[13])) \
+        .flatMap(lambda x: splitSentence(x.split("\t")[13]))
+    print("total num of sentences in negative reviews:", Negative_Reviews.count())
 
+    # Positive_Reviews_collect = Positive_Reviews.collect()
 
-    print(Negative_Reviews.count())
+    from pyspark.mllib.feature import HashingTF, IDF
 
-    Positive_Reviews_collect = Positive_Reviews.collect()
+    # Load documents (one per line).
 
-    #Negative_Reviews_collect = Negative_Reviews.collect()
+    hashingTF = HashingTF(1000)
+    tf = hashingTF.transform(Positive_Reviews)
+    # tf.cache()
+    idf = IDF().fit(tf)
+    tfidf = idf.transform(tf)
+    # print("the shape of tfidf matrix, row represents num of sentences, column represents num of attributions",np.array(tfidf.collect()).shape)
+    # print(tfidf.collect())
+    sb = tfidf.map(lambda x: x.numNonzeros())
+    # print("each sentence's num of nonzero element: ", sb.collect())
+    print("In total there are ", np.sum(np.array(sb.collect())), "nonzero element in tfidf matrix")
 
-    # Negative_Reviews_collect = Negative_Reviews_collect.reshape()
-    # print(type(np.array(Positive_Reviews.collect())[0]))
+    tfidf = tfidf.zipWithIndex()  # .cache()
+    tfidf = tfidf.flatMap(explode).cache()
 
-    # for i in np.array(Positive_Reviews.collect()):
-    #    print(i)
+    # print("tfidf shape after coordinate all the nonzero element: ", np.array(tfidf.collect()).shape)
+    print(tfidf.collect())
 
+    tfidf_T = tfidf.map(lambda x: (x[1], x[0], x[2]))
+    # print(tfidf_T.collect())
 
-    vector_set = np.array([])
-    with tf.Session() as session:
-        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-        index = 0
-        date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    dot_product_matrix = CoordinateMatrix(tfidf_T)
+    #print(dot_product_matrix.toBlockMatrix().toLocalMatrix())
+    cosine_similarity = dot_product_matrix.toIndexedRowMatrix().columnSimilarities()  # .toBlockMatrix().toLocalMatrix().toArray()
+    #print(cosine_similarity.entries.collect())
+    print(cosine_similarity.numCols(), cosine_similarity.numRows())
+    # print(cosine_similarity.toBlockMatrix().toLocalMatrix())
+    # print(cosine_similarity.entries.collect())
+    NP_cos_sim = cosine_similarity.toBlockMatrix().toLocalMatrix().toArray().copy()
+    # .entries.filter(lambda x: x.value > 1e-6) \
+    # .map(lambda x: (x.i, x.j, 1 - x.value)) \
+    # .map(lambda x: ((x[0], 0), x[2])) \
+    # .reduceByKey(lambda x, y: x + y) \
+    # .map(lambda x: (x[0][0],0,x[1]))
+    # .map(lambda x: (0, x[2]))\
+    # .reduceByKey(lambda x, y: x + y) \
+    # .map(lambda x: x[1] * 2 / (num_Postive_Sentence * (num_Postive_Sentence - 1)))  # .saveAsTextFile("output5.txt")
 
-        message_embeddings = np.array(session.run(embed(Positive_Reviews_collect)))
-        print(message_embeddings.shape)
-        vector_set = message_embeddings.copy()
+    indice = np.arange(NP_cos_sim.shape[0])
+    for i in indice:
+        NP_cos_sim[i, i] = 1
+    cosine_distance = 1 - NP_cos_sim
+    print(np.sum(np.triu(cosine_distance))*2/(num_Postive_Sentence * (num_Postive_Sentence - 1)))
+    #print(cosine_distance.mean())
+    # print(type(dot_product_matrix))
+    #f = open("mzz.txt", "w")
+    # f.write(str(CoordinateMatrix(cosine_distance).toBlockMatrix().toLocalMatrix().toArray()))
+    #f.write(str(cosine_distance))
+    # print(cosine_distance.count())
 
-        #print(date_time, ": ", "Num: ", index, message_embeddings.shape)
-        #vector_set = np.append(vector_set, message_embeddings[i, :])
-    vector_set = vector_set.reshape(-1, 512)
-
-    norm_vector = np.sqrt(np.sum(vector_set ** 2, axis=1))[:, np.newaxis]
-
-    norm_matrix = norm_vector.dot(norm_vector.T)
-    dot_product_matrix = vector_set.dot(vector_set.T)
-    cosinedistance_matrix = 1 - dot_product_matrix / norm_matrix
-
-    # print(cosinedistance_matrix.shape)
-    # print(cosinedistance_matrix)
-
-    distance_for_each = (np.sum(cosinedistance_matrix, axis=1) / cosinedistance_matrix.shape[1])[:, np.newaxis]
-    distance_overall = np.mean(distance_for_each)
-    # print(distance)
-    # print(distance.shape)
-    end = time.time()
-    time_spent = end - start
-    '''
-        for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
-            print("Message: {}".format(Positive_Reviews[i]))
-            print("Embedding size: {}".format(len(message_embedding)))
-            message_embedding_snippet = ", ".join(
-                (str(x) for x in message_embedding[:3]))
-            print("Embedding: [{}, ...]\n".format(message_embedding_snippet))
-    '''
-    f = open("Stage3.txt", 'w')
-    # f.write(str(Positive_Reviews.collect()))
-    f.write("total time spent: " + str(time_spent) + "s" + "\n")
-    f.write("Average distance between one sentence with others" + "\n" + str(distance_for_each) + "\n")
-    f.write("Overall Average distance: " + str(distance_overall))
-    f.close()
+    # print()fil
