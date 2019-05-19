a79f42050a336cf4064fc182313bbb932a1a54d4
zmen5809 <zmen5809@gmail.com>
2019-05-20 05:41:20 +1000

    the boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@
 sudo yum -y install git
 sudo pip install --quiet tensorflow-hub
 sudo aws s3 cp s3://comp5349-zmen5809/trouble /home/hadoop/ --recursive
+git clone https://github.com/waystogetthere/COMP5349-Assignment2.git
